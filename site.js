a2088966792abeb95a40ce80849d6be1aba89870
Zairja Llull <judeau@gmail.com>
2023-02-26 20:57:47 -0600

    Add files via upload
    initial commit

---
+++ site.js
@@ -0,0 +1,9 @@
+var wedding = new Date('08-04-2023');
+    var today = new Date();
+
+    if (wedding >= today) {
+      var diff = wedding.getTime() - today.getTime();
+      var daydiff = diff / (1000 * 60 * 60 * 24);
+      var countdown = document.getElementById("header-countdown");
+      countdown.innerText = Math.round(daydiff) + " DAYS TO GO!";
+    }
