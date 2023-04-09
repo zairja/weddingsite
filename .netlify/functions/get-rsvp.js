06dabac93c62f10072040e45fb10ecfe18d61bde
Mark Lagunez <mark.lagunez@outlook.com>
2023-04-09 17:55:48 -0600

    Fixed countdown JS error. Organized files. Added faunaDB support. Added RSVP code functionality.

---
+++ .netlify/functions/get-rsvp.js
@@ -0,0 +1,41 @@
+const faunadb = require('faunadb');
+exports.handler = async (event) => {
+  const q = faunadb.query;
+  const client = new faunadb.Client({
+    secret: process.env.FAUNA_SECRET_KEY,
+  });
+
+  const { code } = event.queryStringParameters;
+  if (!code) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: '❌ Please provide a code.',
+      }),
+    };
+  }
+
+  const doesDocExist = await client.query(
+    q.Exists(q.Match(q.Index('guest_by_code'), code))
+  );
+
+  if (!doesDocExist) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: '❌ That code is wrong.',
+      }),
+    };
+  }
+
+  const document = await client.query(
+    q.Get(q.Match(q.Index('guest_by_code'), code))
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      name: document.data.name,
+    }),
+  };
+};
