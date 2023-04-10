b2f1d444e04d6119d309f0ea58ea371fd8d95493
Mark Lagunez <mark.lagunez@outlook.com>
2023-04-09 23:22:36 -0600

    Alerts after saving changes. Document updated in DB. CSS tweaks.

--- .netlify/functions/update-rsvp.js
+++ .netlify/functions/update-rsvp.js
@@ -5,48 +5,62 @@ exports.handler = async (event, context) => {
     secret: process.env.FAUNA_SECRET_KEY,
   });
 
-  console.log(event.body);
-
   const eventBody = JSON.parse(event.body);
 
-  console.log(eventBody)
-
-  const code = eventBody.code;
-
-  console.log(code)
-
-  if (!code) {
+  if (!eventBody.code) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: 'Please provide a code.',
+        message: 'There\'s a problem with your RSVP.',
       }),
     };
   }
 
   const doesDocExist = await client.query(
-    q.Exists(q.Match(q.Index('guest_by_code'), code))
+    q.Exists(q.Match(q.Index('guest_by_code'), eventBody.code))
   );
 
   if (!doesDocExist) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-        message: 'That code is wrong.',
+        message: 'There\'s a problem with your RSVP.',
       }),
     };
   }
 
-  // update here (do q.update)
-
   const document = await client.query(
-    q.Get(q.Match(q.Index('guest_by_code'), code))
+    q.Get(q.Match(q.Index('guest_by_code'), eventBody.code))
+  );
+
+  // add 'simple' form data to the object we'll use to update the document
+  var newDocumentData = {
+    isGoing: (eventBody.isGoing == 'on' ? true : false),
+    arrivalDate: eventBody.arrivalDate,
+    stayLocation: eventBody.stayLocation,
+    mailingAddress: eventBody.mailingAddress,
+    song: eventBody.song,
+    entrees_1 : eventBody.entrees_1
+  }
+
+  // add entree and guest name info
+  for (let i = 0; i < document.data.maxGuests; ++i) {
+    let nameKey = "guest_" + (i+2)
+    let entreeKey = "entrees_" + (i+2)
+    newDocumentData[nameKey] = eventBody[nameKey]
+    newDocumentData[entreeKey] = eventBody[entreeKey]
+  }
+
+  await client.query(
+    q.Update(document.ref, {
+      data: newDocumentData,
+    })
   );
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Your RSVP was updated!"
+      message: "✔️ Your RSVP was updated!"
     }),
   };
 };
